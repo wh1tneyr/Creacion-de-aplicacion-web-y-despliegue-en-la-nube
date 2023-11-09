7d343ef636881e3a58cef16589830a830b56bab8
Whitney Rios <rioswhit@gmail.com>
2023-11-08 23:31:00 -0300

    cambié al nuevo entorno virtual vehicle_venv y modificamos ruta relativa dl archivo

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
-car_data = pd.read_csv('../vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
 
 st.header('Autos a la venta')
-        
-if hist_button: # al hacer clic en el botón
+st.write('Aplicación en construcción')
+
+if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
-        
+
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
